654f454e169f1a3fe72a0c088fb56187f0027a50
SahuShantanu <132589922+SahuShantanu@users.noreply.github.com>
2023-10-04 22:29:49 +0530

    Create area_of_triangle.c

---
+++ area_of_triangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    
+    float h,b;
+  
+    printf("enter the height of triangle :\n");
+    scanf("%f",&h);
+    printf("enter the base of triangle :\n");
+    scanf("%f",&b);
+    printf("your area of triangle is %.2f.",0.5*b*h);
+  
+    return 0;
+}
