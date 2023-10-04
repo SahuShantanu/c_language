0179305417c041db1e63db004e653f1c2962ab26
SahuShantanu <132589922+SahuShantanu@users.noreply.github.com>
2023-10-04 22:00:17 +0530

    Create add_sub_multi_div_.c

---
+++ add_sub_multi_div_.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+        int main(){
+          int a,b,c,d,e,f,g,h;
+          printf("enter first number for addition");
+          scanf("%d",&a);
+            printf("enter second number for addition");
+  scanf("%d",&b);
+  printf("your addition is %d"a+b);
+printf("enter first number for subtraction");
+          scanf("%d",&c);
+            printf("enter second number for subtraction");
+  scanf("%d",&d);
+  printf("your subtraction is %d"c-d);
+                printf("enter first number for multiplication");
+          scanf("%d",&e);
+            printf("enter second number for multiplication");
+  scanf("%d",&f);
+  printf("your multiplication is %d"e*f);
+                printf("enter first number for division");
+          scanf("%d",&g);
+            printf("enter second number for division");
+  scanf("%d",&h);
+  printf("your division is %d"g/h);
+ return 0;
+        }
