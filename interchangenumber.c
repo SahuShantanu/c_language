074f4a2cfff95bc44de999c1bbfaf4201259fae2
SahuShantanu <132589922+SahuShantanu@users.noreply.github.com>
2023-10-04 22:16:03 +0530

    Create interchangenumber.c

---
+++ interchangenumber.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+    int main(){
+        
+    int a=5,b=7,c;
+    
+    c=a;
+    a=b;
+    b=c;
+      
+    printf("a = %d",a);
+    printf("b = %d",b);
+    return 0;
+    
+              }
