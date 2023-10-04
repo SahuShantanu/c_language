6564c67268965299edc10f1fac9b1303655376f7
SahuShantanu <132589922+SahuShantanu@users.noreply.github.com>
2023-10-05 00:36:50 +0530

    Create check_character_are_capital_small_digit_any_special_character.c

---
+++ check_character_are_capital_small_digit_any_special_character.c
@@ -0,0 +1,22 @@
+//#include<stdio.h>
+//#include<ctype.h>
+//int main()
+// error coming
+ //   {
+ //       char a;
+   //     printf("enter your character :");
+ //       scanf("%c",&a);
+   //    if(a>=65 && a<=91)
+        
+    //        printf("capital letter");
+        
+      //  else if(a>=97 && a<=122)
+    //    
+      //      printf("small letter");
+        //
+    //    else if(a>=27 && a<=47)
+    //    
+         //   printf("special character");
+        
+     //   return 0;
+  //  }
