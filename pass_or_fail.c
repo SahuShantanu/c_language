dc6144272683c6af31f85604d5d99201f452fff8
SahuShantanu <132589922+SahuShantanu@users.noreply.github.com>
2023-10-04 23:31:32 +0530

    Create pass_or_fail.c

---
+++ pass_or_fail.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+    int marks;
+    printf("enter your marks :\n");
+    scanf("%d",&marks);
+     
+     if(marks<=33){
+         printf("you are fail");
+     }
+     else{
+         printf("you are pass");
+     }
+    return 0;
+}
