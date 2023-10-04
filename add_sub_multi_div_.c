93dcf36077192e62bc4fbfe198e8919a6ad28b7a
SahuShantanu <132589922+SahuShantanu@users.noreply.github.com>
2023-10-04 22:08:07 +0530

    Update add_sub_multi_div_.c

--- add_sub_multi_div_.c
+++ add_sub_multi_div_.c
@@ -1,25 +1,25 @@
 #include<stdio.h>
         int main(){
-          int a,b,c,d,e,f,g,h;
-          printf("enter first number for addition");
-          scanf("%d",&a);
-            printf("enter second number for addition");
-  scanf("%d",&b);
-  printf("your addition is %d"a+b);
-printf("enter first number for subtraction");
-          scanf("%d",&c);
-            printf("enter second number for subtraction");
-  scanf("%d",&d);
-  printf("your subtraction is %d"c-d);
-                printf("enter first number for multiplication");
-          scanf("%d",&e);
-            printf("enter second number for multiplication");
-  scanf("%d",&f);
-  printf("your multiplication is %d"e*f);
-                printf("enter first number for division");
-          scanf("%d",&g);
-            printf("enter second number for division");
-  scanf("%d",&h);
-  printf("your division is %d"g/h);
- return 0;
-        }
+        int a,b,c,d,e,f,g,h;
+        printf("enter first number for addition\n");
+        scanf("%d",&a);
+        printf("enter second number for addition\n");
+        scanf("%d",&b);
+        printf("your addition is %d\n",a+b);
+        printf("enter first number for subtraction\n");
+        scanf("%d",&c);
+        printf("enter second number for subtraction\n");
+        scanf("%d",&d);
+        printf("your subtraction is %d\n",c-d);
+        printf("enter first number for multiplication\n");
+        scanf("%d",&e);
+        printf("enter second number for multiplication\n");
+        scanf("%d",&f);
+        printf("your multiplication is %d\n",e*f);
+        printf("enter first number for division\n");
+        scanf("%d",&g);
+        printf("enter second number for division\n");
+        scanf("%d",&h);
+        printf("your division is %d\n",g/h);
+return 0;
+                }
