ef22769088cb7a23f48ef9134109b434b3aa780a
SahuShantanu <132589922+SahuShantanu@users.noreply.github.com>
2023-10-04 22:59:04 +0530

    Create convert_celsius_to_fahrenheit.c

---
+++ convert_celsius_to_fahrenheit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    
+    float c;
+    printf("enter the celsius :\n");
+    scanf("%.1f",&c);
+    printf("your fahrenheit is %.2f",1.8*c+32);
+    return 0;
+}
